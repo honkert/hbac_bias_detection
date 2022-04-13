5a2360fefbf84da107db8fb131cb062a8d9aba81
honkert <immeglaude@live.nl>
2022-04-13 14:47:43 +0200

    added package

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
     name="hbac_bias_detection",
-    version="0.1.9",
+    version="0.2.0",
     packages=find_packages(),
     # package_dir={'hbac_bias_detection': 'src'},
     #package_dir={'': 'hbac_bias_detection'},
